6027fbccada9d929da5f6299883c7afa8349e5bb
Thorsten Ludewig <t.ludewig@gmail.com>
2024-05-19 15:59:42 +0200

    baud rate changed to 115200

--- Projects/NeoPixelRing35/UPLOAD.sh
+++ Projects/NeoPixelRing35/UPLOAD.sh
@@ -5,4 +5,4 @@ source ./bin/activate
 # esptool.py --chip esp8266 --port /dev/ttyUSB0 erase_flash
 esptool.py --chip esp8266 --port /dev/ttyUSB0 --baud 460800 --after hard_reset write_flash --flash_mode dout --flash_size detect 0x0 firmware.bin
 
-picocom --imap lfcrlf -b 74880 /dev/ttyUSB0
+picocom --imap lfcrlf -b 115200 /dev/ttyUSB0
